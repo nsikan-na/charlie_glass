9fe7c34cb7116f309074f2caa2a3de34f10b743c
nsikan.akpan <nsikan.akpan@betmakers.com>
2023-07-19 11:23:32 -0400

    added shadow

--- pages/index.tsx
+++ pages/index.tsx
@@ -393,7 +393,7 @@ const Home = () => {
           />
           <Box
             sx={{
-              background: "rgba(0, 0, 0, .2)",
+              background: "rgba(0, 0, 0, .1)",
               width: "100%",
               height: "100%",
               position: "absolute",
